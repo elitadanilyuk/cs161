a77d9ed05f05d934fbddcff0b3ea854224f7677c
Elita Danilyuk <elitadanilyuk@gmail.com>
2021-02-14 13:28:02 -0700

    adding code for homework 4

---
+++ homework/homework4/Payable.java
@@ -0,0 +1,12 @@
+
+// Payable.java
+// Payable interface declaration.
+/**
+ * @author Elita Danilyuk, S02664865
+ * @verson2021-02-14, CSC-160 Homework 4
+ */
+
+public interface Payable
+{
+    double getPaymentAmount();
+}
